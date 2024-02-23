ebf772854a73fa4793f0dc815ecf18aa9070fc76
WangYunchang <360812146@qq.com>
2024-02-23 17:59:41 +0800

    20240223

--- Blog/app.py
+++ Blog/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file, jsonify, make_response
-from flask_mysqldb import MySQL
-import markdown
-from datetime import datetime
-from PyPDF2 import PdfMerger
-from pathlib import Path
 import os
 import threading
-from io import BytesIO
+from datetime import datetime
+
+import markdown
+from PyPDF2 import PdfMerger
+import pymysql
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify, make_response
 
 extensions = [
     'markdown.extensions.extra',
@@ -19,45 +18,51 @@ extensions = [
 app = Flask(__name__)
 app.secret_key = 'hello world'
 
-# MySQL Configuration
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'a88103882'
-app.config['MYSQL_DB'] = 'blog'
-mysql = MySQL(app)
+conn = pymysql.connect(
+    host='127.0.0.1',  # 主机名（或IP地址）
+    port=3306,  # 端口号，默认为3306
+    user='user_ma',  # 用户名
+    password='a88103882',  # 密码
+    charset='utf8mb4'  # 设置字符编码
+)
+
 
 # Index Page
 @app.route('/')
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT id, title, content, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at FROM posts ORDER BY created_at DESC")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id, title, content, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at FROM posts ORDER BY created_at DESC")
     posts = cur.fetchall()
     cur.close()
     return render_template('index.html', posts=posts)
 
+
 # Render Markdown text
 @app.template_filter('md')
 def markdown_filter(text):
-    return markdown.markdown(text,extensions=extensions)
+    return markdown.markdown(text, extensions=extensions)
+
 
 # Blog Post Page
 @app.route('/post/<int:post_id>')
 def post(post_id):
-    cur = mysql.connection.cursor()
+    cur = conn.cursor()
     cur.execute("SELECT * FROM posts WHERE id = %s", (post_id,))
     post = cur.fetchone()
     if post:
-        post_content = markdown.markdown(post[2],extensions=extensions)
+        post_content = markdown.markdown(post[2], extensions=extensions)
         return render_template('post.html', post=post, post_content=post_content)
     else:
         return render_template('404.html'), 404
+
 
 @app.route('/manage', methods=['GET', 'POST'])
 def manage():
     if request.method == 'POST':
         password = request.form['password']
         # Query the database for the administrator password
-        cur = mysql.connection.cursor()
+        cur = conn.cursor()
         cur.execute("SELECT password FROM admin")
         result = cur.fetchone()
         cur.close()
@@ -79,23 +84,26 @@ def manage():
 # ManagePanel
 @app.route('/manage_panel')
 def manage_panel():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT id, title, content, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at FROM posts ORDER BY created_at DESC")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id, title, content, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at FROM posts ORDER BY created_at DESC")
     posts = cur.fetchall()
     cur.close()
     return render_template('manage_panel.html', posts=posts)
 
+
 # ManagePost
 @app.route('/manage_post/<int:post_id>')
 def manage_post(post_id):
-    cur = mysql.connection.cursor()
+    cur = conn.cursor()
     cur.execute("SELECT * FROM posts WHERE id = %s", (post_id,))
     post = cur.fetchone()
     if post:
-        post_content = markdown.markdown(post[2],extensions=extensions)
+        post_content = markdown.markdown(post[2], extensions=extensions)
         return render_template('manage_post.html', post=post, post_content=post_content)
     else:
         return render_template('404.html'), 404
+
 
 # Add Post
 @app.route('/add_post', methods=['GET', 'POST'])
@@ -104,37 +112,40 @@ def add_post():
         title = request.form['title']
         content = request.form['content']
         created_at = datetime.now()  # Get current datetime
-        cur = mysql.connection.cursor()
+        cur = conn.cursor()
         cur.execute("INSERT INTO posts (title, content, created_at) VALUES (%s, %s, %s)", (title, content, created_at))
-        mysql.connection.commit()
+        conn.commit()
         # flash('Post added successfully', 'success')
         return redirect(url_for('manage_panel'))
     return render_template('add_post.html')
+
 
 # Delete Post
 @app.route('/delete_post/<int:post_id>', methods=['POST'])
 def delete_post(post_id):
     if request.method == 'POST':
-        cur = mysql.connection.cursor()
+        cur = conn.cursor()
         cur.execute("DELETE FROM posts WHERE id = %s", (post_id,))
-        mysql.connection.commit()
+        conn.commit()
         # flash('Post deleted successfully', 'success')
         return redirect(url_for('manage_panel'))
     return 'Method Not Allowed', 405
 
-#Tool Box
+
+# Tool Box
 @app.route('/toolbox')
 def toolbox():
     # Dummy data for tool cards (replace with actual data)
     return render_template('toolbox.html')
 
-#Tool1
+
+# Tool1
 @app.route('/tool1')
 def tool1():
     return render_template('tool1.html')
 
 
-#merge PDF
+# merge PDF
 @app.route('/merge_pdf', methods=['POST'])
 def merge_pdf():
     if 'folder[]' not in request.files:
@@ -178,5 +189,6 @@ def download_merged_pdf():
 
     return response
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
